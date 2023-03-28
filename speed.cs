cb360accaa6e02f8195082af786141d6a6698b47
Mustafa Aydemir <radoingofficial@gmail.com>
2023-03-28 21:59:56 +0300

    Yürüme fonksiyonu için tanımlamalar ve caching işlemi yapıldı

---
+++ speed.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class charactercontroller : MonoBehaviour
+{
+public flouat speed = 0.0f;
+private Rigidbody2D r2d;
+private Animator _animator;
+private
+}
