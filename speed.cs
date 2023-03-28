eb7baf8ce4b607c515546f17d39a90580dfcc4ad
Mustafa Aydemir <radoingofficial@gmail.com>
2023-03-28 22:08:11 +0300

    Düzenleme ve caching yapıldı

--- speed.cs
+++ speed.cs
@@ -4,8 +4,25 @@ using UnityEngine;
 
 public class charactercontroller : MonoBehaviour
 {
-public flouat speed = 0.0f;
+public float speed = 0.0f;
 private Rigidbody2D r2d;
 private Animator _animator;
-private
+private Vector3 charPos;
+[SerializedField] private GameObject _camera;
+
+void Start()
+{
+    r2d = GetComponent<Rigidbody2D>(); // caching r2d
+    _animator = GetComponent<Animator>(); // caching anim
+    charPos = transform.position;
 }
+
+private void FixedUpdate()
+{
+    //r2d.velocity = new Vector2(speed, 0f);
+    _camera.transform.position= new Vector3 (charPos.x,charPos.y,charPos.z - 1.0f);
+}
+
+
+}
+
